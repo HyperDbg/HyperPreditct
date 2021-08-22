4c555bb81a0793821663f733efa6b8774bec9a1f
SinaKarvandi <ms.karvandi@yahoo.com>
2021-08-23 02:04:08 +0430

    change conversion

--- SyscallNameConversion.py
+++ SyscallNameConversion.py
@@ -1,6 +1,23 @@
 import csv
 
-with open('.\\windows-syscalls\\x64\\csv\\nt.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row[0] + " : " + row[30])
+
+def readAllFunctions():
+    with open('.\\windows-syscalls\\x64\\csv\\nt.csv', newline= '') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(row[0] + " : " + row[30])
+
+def convertSyscallNumberToFunctionName(SyscallNum):
+
+    with open('.\\windows-syscalls\\x64\\csv\\nt.csv', newline= '') as f:
+        listOfSyscalls = csv.reader(f)
+        for row in listOfSyscalls:
+            if row[30] == SyscallNum :
+                    return row[0]
+
+# Test
+with open('.\\windows-syscalls\\x64\\csv\\nt.csv', newline= '') as f:
+    reader2 = csv.reader(f)
+    for row in reader2:
+        print(convertSyscallNumberToFunctionName(str(row[30])))
+        #print(str(row[30]))
